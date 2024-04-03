3fa6b6e0ba2f8de3ec83e0f0460b61b8bd50c1c9
Radek Šťasta <radek.stasta@gmail.com>
2024-04-03 16:45:01 +0200

    Fixed restart eww command

--- dotfiles/eww/actions/restart_eww.py
+++ dotfiles/eww/actions/restart_eww.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Define the command to be run, add output redirection to /tmp/kitty_output.txt
-command = 'kitty fish -c "eww close-all && eww open topbar0 && eww open topbar1 && eww open topbar2 && eww open steam; fish -c \'echo Press any key to continue...; read x\'"'
+command = "eww close-all && eww open topbar0 && eww open topbar1 && eww open topbar2 && eww open steam"
 
 # Use subprocess.Popen to execute the command in a new terminal
 subprocess.Popen(command, shell=True)
