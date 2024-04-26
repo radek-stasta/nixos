e385448eed1ad9bd4f18d78676377e7f2b47cc61
Radek Šťasta <radek.stasta@gmail.com>
2024-04-26 10:05:32 +0200

    Added Honkers Railway Launcher

--- flake.nix
+++ flake.nix
@@ -9,21 +9,24 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    aagl = {
+      url = "github:ezKEa/aagl-gtk-on-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, aagl, ... }@inputs: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
         ./hosts/default/configuration.nix
         inputs.home-manager.nixosModules.default
-      ];
-    };
-    nixosConfigurations.work = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./hosts/work/configuration.nix
-        inputs.home-manager.nixosModules.default
+        {
+          imports = [ aagl.nixosModules.default ];
+          nix.settings = aagl.nixConfig; # Set up Cachix
+          programs.honkers-railway-launcher.enable = true;
+        }
       ];
     };
   };
