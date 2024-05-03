6e25eedc026b820408c99a9a862c93c3dc9d6c6a
Radek Šťasta <radek.stasta@gmail.com>
2024-05-03 08:32:04 +0200

    Fixed eww, removed chrome fix

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -148,12 +148,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    (google-chrome.override {
-      commandLineArgs = [
-        "--enable-features=UseOzonePlatform"
-        "--ozone-platform=wayland"
-      ];
-    })
+    google-chrome
     git
     jetbrains.webstorm
     neovim
