16a1cea56bef40b825f575a4d3e802512ce7bc8f
Radek Šťasta <radek.stasta@gmail.com>
2024-07-29 18:03:33 +0200

    Added sync mode to KDE, default session X11

--- modules/nixos/kde.nix
+++ modules/nixos/kde.nix
@@ -2,14 +2,15 @@
 
 {
   imports = [
+    ./nvidia_sync.nix
   ];
 
   config = {
     services = {
       xserver.enable = true;
-      displayManager.sddm = {
-        enable = true;
-        wayland.enable = true;
+      displayManager = {
+        sddm.enable = true;
+        defaultSession = "plasmax11";
       };
       desktopManager.plasma6.enable = true;
     };
