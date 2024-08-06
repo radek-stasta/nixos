3609f62dd22bf9b71a2306d0cedd80aec0f6cfee
Radek Šťasta <radek.stasta@gmail.com>
2024-08-06 20:58:01 +0200

    Styled workspace switcher, added keyboard shortcuts for workspaces

--- modules/home-manager/xfce.nix
+++ modules/home-manager/xfce.nix
@@ -34,8 +34,13 @@
 
         # Settings (needs to be out of store link so changes are reflected in dotfiles)
         "${config.home.homeDirectory}/.config/xfce4/xfconf/xfce-perchannel-xml" = {
-          source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/dotfiles/shared/xfce";
+          source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/dotfiles/shared/xfce/xfconf";
           recursive = true;
+        };
+
+        # Gtk styles for panel (needs to be out of store link so changes are reflected in dotfiles)
+        "${config.home.homeDirectory}/.config/gtk-3.0/gtk.css" = {
+          source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/dotfiles/shared/xfce/gtk3/gtk.css";
         };
 
         # Rofi
