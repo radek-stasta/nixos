a94da1f9ae1bbe13f063c5b498be74e03d830a48
Radek Šťasta <radek.stasta@gmail.com>
2024-08-06 13:22:18 +0200

    Symlinking xfce config files instead of declaring in module (should reflect changes in gui)

--- modules/home-manager/xfce.nix
+++ modules/home-manager/xfce.nix
@@ -25,24 +25,10 @@
         "${config.home.homeDirectory}/.themes/orchis-dark-nord" = {
           source = ../../dotfiles/shared/themes/orchis-dark-nord;
         };
-      };
-    };
 
-    xfconf = {
-      enable = true;
-      settings = {
-        xsettings = {
-          "Net/ThemeName" = "orchis-dark-nord";
-          "Xfce/SyncThemes" = true;
-        };
-        xfwm4 = {
-          "general/theme" = "orchis-dark-nord";
-        };
-        xfce4-keyboard-shortcuts = {
-          "commands/custom/F9" = "tdrop -ma alacritty";
-        };
-        xfce4-desktop = {
-          "backdrop/screen0/monitorVirtual-1/workspace0/last-image" = "${config.home.homeDirectory}/.local/share/wallpapers/nord/nord.jpeg";
+        # Settings
+        "${config.home.homeDirectory}/.themes/orchis-dark-nord" = {
+          source = config.lib.file.mkOutOfStoreSymlink ../../dotfiles/shared/xfce;
         };
       };
     };
