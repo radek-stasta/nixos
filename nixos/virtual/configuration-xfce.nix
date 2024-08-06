f027c49b8961f097de749183e29f620ed0f7e252
Radek Šťasta <radek.stasta@gmail.com>
2024-08-06 14:00:33 +0200

    Added home manager backup file extension, removed bottom panel, emptied top panel

--- nixos/virtual/configuration-xfce.nix
+++ nixos/virtual/configuration-xfce.nix
@@ -24,6 +24,7 @@
 
   home-manager = {
     extraSpecialArgs = { inherit inputs outputs; };
+    backupFileExtension = "backup";
     users = {
       rstasta = import ../../home-manager/virtual/home-xfce.nix;
     };
